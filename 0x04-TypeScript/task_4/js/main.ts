7cacd340661482afaa55c036518092964ff27cdf
Damaris Chege <deenyashke@gmail.com>
2025-11-09 21:54:09 +0300

    Refactor subject instantiation and teacher export

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,12 +1,14 @@
 /// <reference path="./subjects/Teacher.ts" />
 /// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
-export const cTeacher: Subjects.Teacher = {
+export const cTeacher: Teacher = {
     firstName: 'John',
     lastName: 'Doe',
     experienceTeachingC: 10
